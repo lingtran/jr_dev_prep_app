0fbf9a677feb2fdf77931473fc5e11970a1880a4
lingtran <lxiutran@gmail.com>
2016-04-13 17:56:00 -0600

    random questions implemented

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def index
-    render(text: "Only index page for now. Hit return to move on to next question. Want to add timer.")
+    @question = Question.random
   end
 
 end
