cc6181691a80c2265e48d7a9e9b8743e43d1f524
lingtran <lxiutran@gmail.com>
2016-04-14 23:20:49 -0600

    Add to users resources and Users controller
    Add users#create and private method #user_params
    Add redirect to login_path upon creation of account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :questions, only: [:index]
-
+  resources :users, only: [:index, :create]
   get 'users/new', to: "users#new", as: :create_user
 end
--- spec/features/new_junior_dev_user_creates_account_spec.rb
+++ spec/features/new_junior_dev_user_creates_account_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Junior dev user creates an account" do
   scenario "they can log in upon creation" do
-    user = User.create(name: "Felicity", username: "felicity", email: "felicity@fake", password: "felicity")
+    user = { name: "Felicity",
+            email: "felicity@fake",
+            username: "felicity",
+            password: "felicity"
+          }
 
     visit root_path
 
@@ -11,14 +15,13 @@ RSpec.feature "Junior dev user creates an account" do
     end
 
     click_link("Get Login")
-
     assert_equal create_user_path, current_path
 
     within(".registration") do
-      fill_in("Name", with: "#{user.name}")
-      fill_in("E-mail", with: "#{user.email}")
-      fill_in("Set a username", with: "#{user.username}")
-      fill_in("Set a password", with: "#{user.password}")
+      fill_in("Name", with: "#{user[:name]}")
+      fill_in("E-mail", with: "#{user[:email]}")
+      fill_in("Set a username", with: "#{user[:username]}")
+      fill_in("Set a password", with: "#{user[:password]}")
     end
     click_button("Create Login")
 
