650a06ba032d75d185e8ce67202490e424b17653
lingtran <lxiutran@gmail.com>
2016-04-16 12:01:25 -0600

    Pass feature test for user to start a prep session
    Pass the feature test for a user to start a prep session
    Add to factory for questions
    Create singular resource for profile that routes to users#show
    Create users#show view
    Add sessions#create to enable login for an existing user
    Add link for Login on home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :questions, only: [:index]
 
   resources :users, only: [:index, :create]
+  get '/profile', to: "users#show"
+
   get 'users/new', to: "users#new", as: :create_user
 
   get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
 end
