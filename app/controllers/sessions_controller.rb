4b9a347720295994584adbc6e00b735267901ec0
lingtran <lxiutran@gmail.com>
2016-04-18 10:26:14 -0600

    Add peer review feedback
    Add markdown with peer eval

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:session][:username])
-    if @user && @user.role == "admin" && @user.authenticate(params[:session][:password])
+    if @user && @user.admin? && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to admin_questions_path
     elsif @user && @user.authenticate(params[:session][:password])
